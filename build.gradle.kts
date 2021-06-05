86bda66f76b562f9615551ded7e3f1ac574ff3f6
Georgii <49598870+corradowaver@users.noreply.github.com>
2021-06-05 13:09:38 +0300

    coroutines dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
