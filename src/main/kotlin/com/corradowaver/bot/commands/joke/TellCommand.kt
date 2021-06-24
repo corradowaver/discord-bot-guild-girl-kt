377b5f86f2ef8f65e9dd0b3ade6a5b87fe43c58e
corradowaver <corradowave1@gmail.com>
2021-06-24 21:12:41 +0300

    Now only one type of jokes :c

--- src/main/kotlin/com/corradowaver/bot/commands/joke/TellCommand.kt
+++ src/main/kotlin/com/corradowaver/bot/commands/joke/TellCommand.kt
@@ -1,6 +1,7 @@
 package com.corradowaver.bot.commands.joke
 
 import com.corradowaver.bot.commands.Command
+import com.corradowaver.bot.commands.joke.JokeType.CLASSIC
 import com.corradowaver.bot.commands.joke.TellTimerHandler.lastJokeSentTimestamp
 import com.corradowaver.bot.sound.wrappers.MusicHandler
 import com.corradowaver.bot.tts.TextToSpeechConvertor
@@ -15,8 +16,8 @@ class TellCommand(
   val ttsConvertor: TextToSpeechConvertor
 ) : Command {
 
-  override val caller: String = "tell"
-  override val description = "Supporting 3 types of jokes: 'anek', 'story', 'poem' and 18+ tag"
+  override val caller: String = "joke"
+  override val description = "Tells you an amazing story from her life"
   override lateinit var event: GuildMessageReceivedEvent
 
   override fun invoke(args: List<String>) {
@@ -41,24 +42,7 @@ class TellCommand(
   }
 
   private fun receiveJoke(args: List<String>): String {
-
-    val matchingClassic = setOf("anek", "joke", "анекдот", "анек")
-    val matchingStory = setOf("story", "стори", "история")
-    val matchingPoem = setOf("poem", "стих", "стишок")
-
-    var type = when (true) {
-      args.any { it in matchingClassic } -> JokeType.CLASSIC
-      args.any { it in matchingStory } -> JokeType.STORY
-      args.any { it in matchingPoem } -> JokeType.POEM
-      else -> JokeType.CLASSIC
-    }
-
-    val adult = setOf("adult", "18", "18+", "пошлая", "пошлый")
-    if (args.any { it in adult }) {
-      type = type.adultVersion()
-    }
-
-    val joke = requestJoke(type).responseToText()
+    val joke = requestJoke(CLASSIC).responseToText()
     return if (joke.length < 500) joke else receiveJoke(args)
   }
 
