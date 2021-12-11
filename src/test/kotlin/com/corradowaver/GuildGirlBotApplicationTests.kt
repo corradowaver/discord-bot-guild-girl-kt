4b9688c82a5bdf680bde7705a0996466a89bed92
corradowaver <corradowave1@gmail.com>
2021-12-11 17:56:03 +0300

    configuration class in test

--- src/test/kotlin/com/corradowaver/GuildGirlBotApplicationTests.kt
+++ src/test/kotlin/com/corradowaver/GuildGirlBotApplicationTests.kt
@@ -1,9 +1,11 @@
 package com.corradowaver
 
+import com.corradowaver.bot.GuildGirlBotApplication
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
 
-@SpringBootTest
+@SpringBootTest(classes = [GuildGirlBotApplication::class])
 class GuildGirlBotApplicationTests {
 
     @Test
