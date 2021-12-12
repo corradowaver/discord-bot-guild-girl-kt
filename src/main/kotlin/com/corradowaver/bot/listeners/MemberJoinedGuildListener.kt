1be7e6f766fe82075d0d6ed60b3bc4fe0ebe120b
corradowaver <corradowave1@gmail.com>
2021-12-12 14:40:10 +0300

    static import

--- src/main/kotlin/com/corradowaver/bot/listeners/MemberJoinedGuildListener.kt
+++ src/main/kotlin/com/corradowaver/bot/listeners/MemberJoinedGuildListener.kt
@@ -7,8 +7,8 @@ import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import org.springframework.stereotype.Component
 import java.io.File
-import java.nio.file.Files
 import java.nio.file.Files.copy
+import java.nio.file.Files.createTempDirectory
 import java.nio.file.StandardCopyOption.REPLACE_EXISTING
 
 @Component
@@ -16,7 +16,7 @@ class MemberJoinedGuildListener(
   val s3Manager: S3Manager
 ) : ListenerAdapter() {
 
-  private val tempDirPath = Files.createTempDirectory("guild-girl-greetings")
+  private val tempDirPath = createTempDirectory("guild-girl-greetings")
 
   override fun onGuildVoiceJoin(event: GuildVoiceJoinEvent) {
     val randomS3Greeting = s3Manager.getRandomGreeting()
